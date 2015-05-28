1716794a7228356f2fd7f7f8bd4e28003b8ba90d
Colin Chan <colinchan@lumeh.org>
2015-05-28 08:56:15 -0700

    [install] Fix install script so it actually works

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 
 LINK='ln --symbolic --relative --backup --suffix=.old'
 
-LINK bashrc ~/.bashrc
-LINK gitconfig ~/.gitconfig
-LINK profile ~/.profile
-LINK pythonrc ~/.pythonrc
-LINK tmux.conf ~/.tmux.conf
-LINK vimrc ~/.vimrc
-LINK xinitrc ~/.xinitrc
+$LINK bashrc ~/.bashrc
+$LINK gitconfig ~/.gitconfig
+$LINK profile ~/.profile
+$LINK pythonrc ~/.pythonrc
+$LINK tmux.conf ~/.tmux.conf
+$LINK vimrc ~/.vimrc
