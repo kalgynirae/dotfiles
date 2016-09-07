b83fe22cf79f6de01b6408204d4d5fd0bc7a6ca5
Colin Chan <colinchan@lumeh.org>
2016-09-06 20:09:38 -0700

    [hyperterm] Increase font size

--- hyperterm.js
+++ hyperterm.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 14,
+    fontSize: 20,
 
     // font family with optional fallbacks
     fontFamily: '"Liberation Mono"',
