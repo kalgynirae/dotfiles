0c6a120c79d1317c339c8fa87b0109b99ff94b7a
Colin Chan <colinchan@lumeh.org>
2019-02-04 15:10:23 -0800

    [ipython] Make syntax colors similar to nvim colors

--- ipython_config.py
+++ ipython_config.py
@@ -1,1 +1,16 @@
+from IPython.terminal.prompts import Prompts
+from pygments.style import Style
+from pygments.token import Token
+
+class AnsiStyle(Style):
+    styles = {
+        Token.Comment: "#ansidarkgray",
+        Token.Keyword: "#ansidarkred bold",
+        Token.Literal: "#ansifuchsia",
+        Token.Literal.String: "#ansibrown",
+        Token.Literal.String.Escape: "#ansiteal italic",
+        Token.Name.Decorator: "#ansidarkgreen",
+    }
+
 c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.highlighting_style = AnsiStyle
