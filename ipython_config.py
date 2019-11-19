0aaa4c3e331f8c2f3aabadbd85050af7a3d0e42d
Colin Chan <colinchan@lumeh.org>
2019-11-18 21:07:06 -0800

    [ipython] Comment out style for now
    It's causing the current version of iPython I have on Fedora to throw
    errors.

--- ipython_config.py
+++ ipython_config.py
@@ -2,15 +2,15 @@ from IPython.terminal.prompts import Prompts
 from pygments.style import Style
 from pygments.token import Token
 
-class AnsiStyle(Style):
-    styles = {
-        Token.Comment: "ansibrightblack",
-        Token.Keyword: "ansired bold",
-        Token.Literal: "ansibrightmagenta",
-        Token.Literal.String: "ansiyellow",
-        Token.Literal.String.Escape: "ansicyan italic",
-        Token.Name.Decorator: "ansigreen",
-    }
+#class AnsiStyle(Style):
+#    styles = {
+#        Token.Comment: "ansibrightblack",
+#        Token.Keyword: "ansired bold",
+#        Token.Literal: "ansibrightmagenta",
+#        Token.Literal.String: "ansiyellow",
+#        Token.Literal.String.Escape: "ansicyan italic",
+#        Token.Name.Decorator: "ansigreen",
+#    }
 
 c.TerminalInteractiveShell.confirm_exit = False
-c.TerminalInteractiveShell.highlighting_style = AnsiStyle
+#c.TerminalInteractiveShell.highlighting_style = AnsiStyle
