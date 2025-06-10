2ad03a12b1f4e6c1cf268397e42eab46086e0b3c
Colin Chan <colin+git@lumeh.org>
2025-06-10 11:58:43 -0700

    Add config for niri; update systemd units to work with niri-session

--- install.py
+++ install.py
@@ -64,6 +64,7 @@ configs: dict[str, Output] = {
     ".config/kitty/kitty.conf": render("kitty.conf.jinja"),
     ".config/mako/config": symlink_to("mako"),
     ".config/mpv/mpv.conf": symlink_to("mpv.conf"),
+    ".config/niri/config.kdl": render("niri.kdl.jinja"),
     ".config/nvim": symlink_to("nvim"),
     ".config/paru/paru.conf": symlink_to("paru.conf"),
     ".config/pipewire/pipewire.conf.d/15-netjack2.conf": symlink_to(
