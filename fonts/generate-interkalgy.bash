62d8caf8643ded25a3136b74a5f2fa3fe74720d9
Colin Chan <colin+git@lumeh.org>
2025-11-07 09:35:11 -0800

    generate-interkalgy: Switch from pipenv to uv

--- fonts/generate-interkalgy.bash
+++ fonts/generate-interkalgy.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for suffix in "" "-Italic"; do
-  pipenv run pyftfeatfreeze -v -R 'Inter/Interkalgy' -f cv05,cv08 fonts/InterVariable"$suffix".ttf ~/.local/share/fonts/InterkalgyVariable"$suffix".ttf
+  uv run pyftfeatfreeze -v -R 'Inter/Interkalgy' -f cv05,cv08 fonts/InterVariable"$suffix".ttf ~/.local/share/fonts/InterkalgyVariable"$suffix".ttf
 done
