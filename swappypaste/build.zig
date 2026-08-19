3722a5e1d0e615e1df22751f672cc150039d0348
Colin Chan <colin+git@lumeh.org>
2026-08-19 14:57:00 -0700

    Move swappypaste into ultrapaste and rename it

--- swappypaste/build.zig
+++ swappypaste/build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
-        .name = "ultrapaste",
+        .name = "swappypaste",
         .root_module = b.createModule(.{
             .root_source_file = b.path("main.zig"),
             .target = target,
